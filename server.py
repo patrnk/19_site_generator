89330b01ef777a21a9069da3a4a481e11aa3ce93
Paul Vergeev <paulvergeev@gmail.com>
2017-08-10 11:27:37 +0300

    Add article generation

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import json
-import os.path
+import os
 
+import markdown
 import jinja2
 from livereload import Server
 
 ROOT = 'live_website/'
 TEMPLATES = 'templates/'
+SOURCES_ROOT = 'articles/'
 
 
 def load_article_info_by_topic(path):
@@ -28,9 +30,20 @@ def load_article_info_by_topic(path):
     return topics
 
 
+def read_text_from_file(path):
+    with open(path) as text_file:
+        return text_file.read()
+
+
 def dump_html_to_file(html, path):
     with open(path, 'w') as html_file:
         html_file.write(html)
+
+
+def generate_article_filename(article_source):
+    source_filename = os.path.basename(article_source)
+    article_slug = os.path.splitext(source_filename)[0]
+    return '.'.join((article_slug, 'html'))
 
 
 def render_index(environment, topics):
@@ -43,7 +56,21 @@ def render_index(environment, topics):
 
 
 def render_articles(environment, topics):
-    pass
+    for slug, topic in topics.items():
+        topic_path = os.path.join(ROOT, slug)
+        os.makedirs(topic_path, exist_ok=True)
+        for article in topic['articles']:
+            md_article = read_text_from_file(os.path.join(SOURCES_ROOT, article['source']))
+            html_article = markdown.markdown(md_article)
+            context = {
+                'title': article['title'],
+                'text': html_article,
+                'topic': topic['title'],
+            }
+            rendered_article = environment.get_template('article.html').render(context)
+            article_name = generate_article_filename(article['source'])
+            article_path = os.path.join(topic_path, article_name)
+            dump_html_to_file(rendered_article, article_path)
 
 
 def make_site():
