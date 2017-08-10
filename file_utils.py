833bbc66b20a2f25535c117b4b6f4cf1506c3950
Paul Vergeev <paulvergeev@gmail.com>
2017-08-10 11:33:29 +0300

    Move file utils to separate module

---
+++ file_utils.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+
+def load_article_info_by_topic(path):
+    with open(path) as json_file:
+        article_info = json.load(json_file)
+    topics = {}
+    for topic in article_info['topics']:
+        topics[topic['slug']] = {
+            'title': topic['title'],
+            'articles': []
+        }
+    for article in article_info['articles']:
+        topic_slug = article['topic']
+        topics[topic_slug]['articles'].append(
+            {
+                'title': article['title'],
+                'source': article['source'],
+            }
+        )
+    return topics
+
+
+def read_text_from_file(path):
+    with open(path) as text_file:
+        return text_file.read()
+
+
+def dump_html_to_file(html, path):
+    with open(path, 'w') as html_file:
+        html_file.write(html)
+
+
+def generate_article_filename(article_source):
+    source_filename = os.path.basename(article_source)
+    article_slug = os.path.splitext(source_filename)[0]
+    return '.'.join((article_slug, 'html'))
--- server.py
+++ server.py
@@ -1,49 +1,14 @@
-import json
 import os
 
 import markdown
 import jinja2
 from livereload import Server
 
+import file_utils
+
 ROOT = 'live_website/'
 TEMPLATES = 'templates/'
 SOURCES_ROOT = 'articles/'
-
-
-def load_article_info_by_topic(path):
-    with open(path) as json_file:
-        article_info = json.load(json_file)
-    topics = {}
-    for topic in article_info['topics']:
-        topics[topic['slug']] = {
-            'title': topic['title'],
-            'articles': []
-        }
-    for article in article_info['articles']:
-        topic_slug = article['topic']
-        topics[topic_slug]['articles'].append(
-            {
-                'title': article['title'],
-                'source': article['source'],
-            }
-        )
-    return topics
-
-
-def read_text_from_file(path):
-    with open(path) as text_file:
-        return text_file.read()
-
-
-def dump_html_to_file(html, path):
-    with open(path, 'w') as html_file:
-        html_file.write(html)
-
-
-def generate_article_filename(article_source):
-    source_filename = os.path.basename(article_source)
-    article_slug = os.path.splitext(source_filename)[0]
-    return '.'.join((article_slug, 'html'))
 
 
 def render_index(environment, topics):
@@ -52,7 +17,7 @@ def render_index(environment, topics):
     }
     rendered_index = environment.get_template('index.html').render(context)
     index_path = os.path.join(ROOT, 'index.html')
-    dump_html_to_file(rendered_index, index_path)
+    file_utils.dump_html_to_file(rendered_index, index_path)
 
 
 def render_articles(environment, topics):
@@ -60,7 +25,9 @@ def render_articles(environment, topics):
         topic_path = os.path.join(ROOT, slug)
         os.makedirs(topic_path, exist_ok=True)
         for article in topic['articles']:
-            md_article = read_text_from_file(os.path.join(SOURCES_ROOT, article['source']))
+            md_article = file_utils.read_text_from_file(
+                os.path.join(SOURCES_ROOT, article['source'])
+            )
             html_article = markdown.markdown(md_article)
             context = {
                 'title': article['title'],
@@ -68,13 +35,13 @@ def render_articles(environment, topics):
                 'topic': topic['title'],
             }
             rendered_article = environment.get_template('article.html').render(context)
-            article_name = generate_article_filename(article['source'])
+            article_name = file_utils.generate_article_filename(article['source'])
             article_path = os.path.join(topic_path, article_name)
-            dump_html_to_file(rendered_article, article_path)
+            file_utils.dump_html_to_file(rendered_article, article_path)
 
 
 def make_site():
-    topics = load_article_info_by_topic('config.json')
+    topics = file_utils.load_article_info_by_topic('config.json')
     environment = jinja2.Environment(
         loader=jinja2.FileSystemLoader(TEMPLATES)
     )
